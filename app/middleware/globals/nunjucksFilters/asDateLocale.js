735977064ceebae2a4bb98e4b3074268605d19ff
Swashata Ghosh <swashata@wpquark.com>
2021-05-29 15:49:59 +0530

    feat: fix date on bulk upload and display

---
+++ app/middleware/globals/nunjucksFilters/asDateLocale.js
@@ -0,0 +1,22 @@
+module.exports = (value) => {
+	if(value === null || value === undefined) {
+			return;
+	}
+	const date = new Date(value);
+	const months = {
+        0 : 'Jan',
+        1 : 'Feb',
+        2 : 'Mar',
+        3 : 'Apr',
+        4 : 'May',
+        5 : 'Jun',
+        6 : 'Jul',
+        7 : 'Aug',
+        8 : 'Sep',
+        9 : 'Oct',
+        10 : 'Nov',
+        11 : 'Dec',
+    };
+	let formedDate = `${date.getDate()}-${months[date.getMonth()]}-${date.getFullYear()}`;
+	return formedDate;
+};
